f83d1e6d208a49017a478dc8e7faa13d1345df96
Esmail Sameh <esmil.sameh2005@gmail.com>
2025-02-22 07:16:30 +0200

    first commit

---
+++ src/main/java/com/mycompany/html_editor/Html_editor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.html_editor;
+
+/**
+ *
+ * @author 11
+ */
+public class Html_editor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
