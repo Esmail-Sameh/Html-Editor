b671f7fa294cefa128bc2f7e007c2387775078d9
Esmail Sameh <esmil.sameh2005@gmail.com>
2025-02-22 07:54:44 +0200

    final app

--- src/main/java/com/mycompany/html_editor/Html_editor.java
+++ src/main/java/com/mycompany/html_editor/Html_editor.java
@@ -1,16 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.html_editor;
 
-/**
- *
- * @author 11
- */
+import javax.swing.WindowConstants;
+
+
 public class Html_editor {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FrmPage f = new FrmPage();
+        f.setVisible(true);
+        f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        
     }
 }
